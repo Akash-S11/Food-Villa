44f24ee1353c7e3bf26e69f068956cb1faecdbc8
akash <akashs1132000@gmail.com>
2024-10-28 18:19:10 +0530

    Inception - Creating heading using react

---
+++ Desktop/React/app.js
@@ -0,0 +1,29 @@
+const heading = React.createElement(
+    "h1",
+    {
+        id : "title",
+    },
+    "heading"
+);
+
+const heading1 = React.createElement(
+    "h2",
+    {
+        id : "title",
+    },
+    "heading1"
+);
+
+const container = React.createElement(
+    "div",
+    {
+        id : "container",
+    },
+    [heading, heading1]
+);
+
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
