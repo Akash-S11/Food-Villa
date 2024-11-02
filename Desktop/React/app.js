863bebe4dba79668880f92d5361d7eda44b16c43
akash <akashs1132000@gmail.com>
2024-11-02 23:20:48 +0530

    header with one- body reasturant card display

--- Desktop/React/app.js
+++ Desktop/React/app.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-/*
-    1. Header
-        - Logo
-        - Nav Items
-    2. Body
-        - Search
-        - Restaurant Card
-    3. Footer
-        - Copyright
-        - Links
-        - Address
-        - Contact
-*/
+import ReactDOM, { createRoot } from 'react-dom/client';
+// import Header, { Title } from "./src/components/header";
+// import Body from "./src/components/body";
+// import Footer from "./src/components/footer";
+// import {IMG_CDN_URL} from "./src/components/constants";
 
-//1. Header Component
+const Title = () => (
+    <h1 id = "title" key = "h2">
+    Food Villa </h1>
+);
+
 const Header = () => {
     return <div className="header">
+        {/* <Title /> */}
         <div>
-            <img className="logo" src="https://www.ujudebug.com/wp-content/uploads/2020/05/Screenshot_6.png"/>
+            <img className="logo" src={"https://yt3.ggpht.com/ytc/AKedOLSpK3T_2RxkMYb-pk9oENQB0NvYpeOdXRgQe8i5=s800-c-k-c0x00ffffff-no-rj" }/>
         </div>
         <div className="nav-items">
             <ul>
@@ -29,111 +25,42 @@ const Header = () => {
             </ul>
         </div>
     </div>
-}//Header
+}
 
-const resList = [
-    {
-        id: "1001",
-        name : "Santosh Family Dhaba",
-        receipe: "Tomato Soup",
-        rating: "4.0",
-        price : "₹300 for two",
-        deliveryTime: "22 MINS",
-        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/mkcibe6x1ychvzjkywxk",
-    },
-    {
-        id: "1002",
-        name : "Cafe Niloufer",
-        receipe: "Maska Bun",
-        rating: "4.5",
-        price : "₹400 for two",
-        deliveryTime: "32 MINS",
-        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/dc9t0kiq7lhsd3pndohf",
-    },
-    {
-        id: "1003",
-        name : "Santosh Family Dhaba",
-        receipe: "Crispy Corn",
-        rating: "4.5",
-        price : "₹560 for two",
-        deliveryTime: "32 MINS",
-        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/dwqb8nhiop8fvqya0alq",
-    },
-    {
-        id: "1004",
-        name : "Grameen Kulfi",
-        receipe: "Sitaphal Stick Kulfi",
-        rating: "4.6",
-        price : "₹120 for two",
-        deliveryTime: "18 MINS",
-        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/vu0thsifji9wgfuzfmic",
-    },
-    {
-        id: "1005",
-        name : "Nrs Nandini",
-        receipe: "Idli",
-        rating: "4.4",
-        price : "₹150 for two",
-        deliveryTime: "21 MINS",
-        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/9fa5ca30c3db622c69cec0326ff63b41",
-    },
-    {
-        id: "1006",
-        name : "Utsav Vegetarian",
-        receipe: "Kaju Paneer Biryani",
-        rating: "4.1",
-        price : "₹300 for two",
-        deliveryTime: "32 MINS",
-        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/y2lkcxxax3794vepe2se",
-    },
-    {
-        id: "1007",
-        name : "Subbayya Gari Hotel",
-        receipe: "Butta Bojanam",
-        rating: "4.0",
-        price : "₹300 for two",
-        deliveryTime: "24 MINS",
-        image: "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/iwtocxqjtu57vc7l52h9",
-    },
-]
-
-
-//2. Body Component
-const Body = () => {
-    return <div className="body">
-        <div className="res-search">
-            Search
+const ReasturantCards = () => {
+    return(
+        <div className="card">
+            <img src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/5b9c53df-e0cb-48fc-ac31-f02735592edc_10575.jpg' />
+            <h1>pizza hut</h1>
+            <h3>pizza, Indians</h3>
+            <h4>4.3 stars</h4>
         </div>
-        <div className="res-container">
-            {
-                resList.map((restaurant) => (
-                    <RestaurantCard key={restaurant.id} resData={restaurant}/>
-                ))
-            }
-        </div>
+    )
+}
+const Body = () =>{
+    return 
+    <div>
+        <ReasturantCards />
     </div>
+        
 }
 
-//3. Restaurant Card
-const RestaurantCard = (props) => {
-    const {resData} = props;
-
-    return (<div className="res-card">
-        <img className="res-logo" src={resData.image}/>
-        <h3>{resData.name}</h3>
-        <h4>{resData.receipe}</h4>
-        <h4>{resData.rating}</h4>
-        <h4>{resData.deliveryTime}</h4>
-        <h4>{resData.price}</h4>
-    </div>);
+const Footer = () =>{
+    return <h4>footer</h4>
 }
-
 const AppLayout = () => {
-    return <div className="app">
+    return (
+        <>
         <Header/>
         <Body/>
-    </div>
-}
-const root = createRoot(document.getElementById("root"));
+        <ReasturantCards />
+        <Footer />
+        
+        </>
+    );
+};
 
-root.render(<AppLayout/>);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
