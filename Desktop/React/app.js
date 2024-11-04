c46356a1fec0c9abaf512c2c8fd1628f9a6d843c
akash <akashs1132000@gmail.com>
2024-11-04 17:43:35 +0530

    nested routing using outlet and children

--- Desktop/React/app.js
+++ Desktop/React/app.js
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/header";
 import Body from "./src/components/body";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Footer from "./src/components/footer";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./src/components/about";
 import Contact from "./src/components/contact";
 import Error from "./src/components/error";
+
 
 const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
+      <Footer />
     </div>
   );
 };
@@ -21,15 +24,22 @@ const AppRouter = createBrowserRouter ([
     path : "/",
     element : <AppLayout />,
     errorElement : <Error />,
+    children : [
+    {
+      path : "/",
+      element: <Body />,
+    },
+    {
+      path : "/about",
+      element: <About />,
+    },
+    {
+      path : "/contact",
+      element : <Contact />
+    }
+
+    ]
   },
-  {
-    path : "/about",
-    element: <About />,
-  },
-  {
-    path : "/contact",
-    element : <Contact />
-  }
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- Desktop/React/src/components/footer.js
+++ Desktop/React/src/components/footer.js
@@ -1,5 +1,7 @@
+
+
 const Footer =() => {
-    return <h4> footer </h4>;
+    return <h4 className="foot"> footer </h4>;
 };
 
 export default Footer;
